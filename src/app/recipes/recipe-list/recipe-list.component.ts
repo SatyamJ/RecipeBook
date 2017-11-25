7ae3e83b10b78418325cb791aaa3a5318b6a0c18
Satyam Jaiswal <snjaiswa@asu.edu>
2017-11-25 11:03:50 -0800

    skeleton with recipe list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
+import { Recipe} from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
@@ -7,7 +9,10 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('Puri bhaji', 'A popular North-Indian cusine', 'http://ichef.bbci.co.uk/food/ic/food_16x9_608/cuisines/indian_16x9.jpg'),
+    new Recipe('Puri bhaji', 'A popular North-Indian cusine', 'http://ichef.bbci.co.uk/food/ic/food_16x9_608/cuisines/indian_16x9.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
